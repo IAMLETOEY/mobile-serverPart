5cb6408c8b0883d6f39bad8841d7c83dd1c18792
letoey <faint77@hotmail.com>
2017-02-05 18:06:34 +0800

    登录功能完成

--- src/projects/app/routes/user/login.js
+++ src/projects/app/routes/user/login.js
@@ -3,7 +3,7 @@ var auth = require(__root + '/src/commons/auth');
 var User = require(__root + '/src/models/User');
 
 module.exports = function (app) {
-    app.post('/app/user/login', function (req, res) {
+    app.post('/user/login', function (req, res) {
         try {
             var reqData = JSON.parse(req.body.data);
         } catch (e) {
@@ -12,19 +12,18 @@ module.exports = function (app) {
         }
 
         User.findOneAsync({
-            loginName: reqData.loginName,
+            account: reqData.account,
             password: reqData.password,
             delFlag: 2
         }, null, {
-            select: 'loginName name type avatar'
+            select: 'account nickname type avatar'
         }).then(function (_user) {
             if (_user) {
                 var redis = require('redis');
                 var client = redis.createClient();
                 var sessionID = req.sessionID;
-                var key = 'AppSessionID:' + sessionID;
-
-                client.keys('AppSessionID:*', function (err, keys) {
+                var key = 'sessionID:' + sessionID;
+                client.keys('sessionID:*', function (err, keys) {
                     _.each(keys, function (key) {
                         client.get(key, function (err, reply) {
                             var user = JSON.parse(reply.toString());
@@ -47,13 +46,11 @@ module.exports = function (app) {
                     }
                 };
                 res.send(resData, resultCode.type, 200);
-                console.log('/app/user/login.do suc:---->', _user);
             } else {
-                console.log('/app/user/login.do no user:---->', _user);
                 res.send(resultCode['50101'], resultCode.type, 200);
             }
         }).catch(function (err) {
-            console.log('/app/user/login.do err:---->', err);
+            console.log('/app/user/login err:---->', err);
             res.send(resultCode['50000'], resultCode.type, 200);
         })
     });
